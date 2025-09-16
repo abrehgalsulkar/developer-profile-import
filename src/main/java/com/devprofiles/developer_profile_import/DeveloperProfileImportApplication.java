38e12d246d645d1cb93e94411f49c9a90af3726d
abrehgalsulkar <123094752+abrehgalsulkar@users.noreply.github.com>
2025-09-16 19:36:21 -0400

    Initial Spring Boot push

---
+++ src/main/java/com/devprofiles/developer_profile_import/DeveloperProfileImportApplication.java
@@ -0,0 +1,13 @@
+package com.devprofiles.developer_profile_import;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperProfileImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperProfileImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devprofiles/developer_profile_import/DeveloperProfileImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprofiles.developer_profile_import;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperProfileImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
